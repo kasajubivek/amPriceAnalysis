ea28ff05c055073e44181635010b1cfd6c14e232
Bivek <bkasaju97@gmail.com>
2023-12-03 02:32:05 -0500

    latestttt 03/12/23

--- src/main/java/features/BestCarDeal.java
+++ src/main/java/features/BestCarDeal.java
@@ -4,31 +4,26 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
 public class BestCarDeal {
 
-    static Set<Double> uniquePrices = new HashSet<>();
-    static List<Car> bestDeals = new ArrayList<>();
+    static Set<Double> uniquePricesNewCars = new HashSet<>();
+    static Set<Double> uniquePricesUsedCars = new HashSet<>();
+    static List<Car> bestDealsNewCars = new ArrayList<>();
+    static List<Car> bestDealsUsedCars = new ArrayList<>();
+    static List<Car> newCars = new ArrayList<>();
+    static List<Car> usedCars = new ArrayList<>();
 
     public static void main(String[] args) {
-//        Car[] cars = {
-//                new Car("Car E", 30000, 32),
-//                new Car("Car G", 30000, 19),
-//                new Car("Car O", 29000, 19),
-//                new Car("Car F", 30000, 21),
-//                new Car("Car B", 18000, 25),
-//                new Car("Car B", 18000, 26),
-//                new Car("Car C", 30000, 28),
-//                new Car("Car D", 30000, 28),
-//                new Car("Car A", 25000, 30),
-//        };
-
 
         List<Car> _all_cars_data_ = new ArrayList<>();
 
-        String[] _cars_data_ = {"src/main/resources/cargurus.ca.csv"};
+        String[] _cars_data_ = {"src/main/resources/driveaxis.ca.csv","src/main/resources/goauto.ca.csv","src/main/resources/cargurus.ca.csv"};
 
         for(String _car_data_: _cars_data_){
             List _data_ = readCarsFromCSV(_car_data_);
@@ -39,24 +34,63 @@ public class BestCarDeal {
         Car[] cars = _all_cars_data_.toArray(new Car[0]);
 
 
-        mergeSort(cars, 0, cars.length - 1, "mileage");
+        mergeSort(cars, 0, cars.length - 1, "fuelConsumption");
         mergeSort(cars, 0, cars.length - 1, "price");
 
-        System.out.println("Cars sorted by mileage and then by price:\n");
+//        System.out.println("Cars sorted by fuelConsumption and then by price:\n");
 
         for (Car car : cars) {
-            System.out.println(car);
+//            System.out.println(car);
 
-            if (!uniquePrices.contains(car.getPrice())) {
-                uniquePrices.add(car.getPrice());
-                bestDeals.add(car);
+            if ("0".equals(car.getMileage())) {
+                newCars.add(car);
+            } else {
+                usedCars.add(car);
             }
         }
 
-        System.out.println("\nBest deals for cars according to the price range and mileage:\n");
-        for (Car car : bestDeals) {
+        System.out.println("\nNew cars:\n");
+        for (Car car : newCars) {
             System.out.println(car);
         }
+
+
+        System.out.println("\nUsed cars:\n");
+        for (Car car : usedCars) {
+            System.out.println(car);
+        }
+
+
+
+        for (Car car : newCars) {
+            if (!uniquePricesNewCars.contains(car.getPrice())) {
+                uniquePricesNewCars.add(car.getPrice());
+                bestDealsNewCars.add(car);
+            }
+        }
+
+        for (Car car : usedCars) {
+            if (!uniquePricesUsedCars.contains(car.getPrice())) {
+                uniquePricesUsedCars.add(car.getPrice());
+                bestDealsUsedCars.add(car);
+            }
+        }
+
+
+        System.out.println("\nBest deals for New cars:\n");
+        for (Car car : bestDealsNewCars) {
+            System.out.println(car);
+        }
+
+        System.out.println("\nBest deals for Used cars:\n");
+        for (Car car : bestDealsUsedCars) {
+            System.out.println(car);
+        }
+
+//        System.out.println("\nBest deals for cars according to the price range and mileage:\n");
+//        for (Car car : bestDeals) {
+//            System.out.println(car);
+//        }
 
     }
 
@@ -83,7 +117,7 @@ public class BestCarDeal {
 
         while(_i_ <= mid && _j_ <= high){
 
-            if(compareBy.equals("mileage")?(_cars_arr_[_i_].getMileage() >= _cars_arr_[_j_].getMileage()):(_cars_arr_[_i_].getPrice() <= _cars_arr_[_j_].getPrice())){
+            if(compareBy.equals("fuelConsumption")?(_cars_arr_[_i_].getFuelConsumption() <= _cars_arr_[_j_].getFuelConsumption()):(_cars_arr_[_i_].getPrice() <= _cars_arr_[_j_].getPrice())){
                 _sorted_cars_[_k_] = _cars_arr_[_i_];
                 _i_++;
             }else {
@@ -119,25 +153,68 @@ public class BestCarDeal {
 
 
     private static List readCarsFromCSV(String filePath) {
+        double price = 0, fuelConsumption = 0;
+        String name = "", priceString, mileage = "", fuelConsumptionString;
         List<Car> cars = new ArrayList<>();
+        Matcher matcher_1, matcher_2;
+        String regex_1 = "([0-9.]+) L/100km", regex_2 = "([0-9.]+) City / ([0-9.]+) Highway";
+        Pattern pattern_1 = Pattern.compile(regex_1);
+        Pattern pattern_2 = Pattern.compile(regex_2);
 
         try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
             List<String[]> records = csvReader.readAll();
 
             // Assuming the CSV structure is: Name, Price, Mileage
-            for (int i = 1; i <= 15 && i < records.size(); i++) {
+            for (int i = 1; i <= 10 && i < records.size(); i++) {
                 String[] record = records.get(i);
 
-                String name = record[0].trim();
-                String priceString = record[1].replaceAll("[^\\d.]+", "");
-                double price = Double.parseDouble(priceString);
-                double mileage = Double.parseDouble(record[2].replaceAll("[^\\d.]+", "").trim());
+                if(record[0] != null && !record[0].contains("No Information Found")){
+                    name = record[0].trim();
+                }
 
-                Car car = new Car(name, price, mileage);
+                if(record[1] != null && !record[0].contains("No Information Found")){
+                    priceString = record[1].replaceAll("[^\\d.]+", "");
+
+                    price = Double.parseDouble(priceString);
+                }
+
+                if(!record[2].contains("New") && !record[2].contains("new") && !record[0].contains("No Information Found") && record[2] != null){
+                    mileage = record[2].replaceAll("[^\\d.]+", "").trim();
+                }else {
+                    mileage = "0";
+                }
+
+                fuelConsumptionString = record[3];
+
+                if(fuelConsumptionString.contains("L/100km")){
+                    matcher_1 = pattern_1.matcher(fuelConsumptionString);
+
+                    if(matcher_1.find()){
+                        fuelConsumption = Double.parseDouble(matcher_1.group(1));
+                    }
+                }else if(fuelConsumptionString.contains("Highway")){
+                    matcher_2 = pattern_2.matcher(fuelConsumptionString);
+
+                    if(matcher_2.find()){
+                        fuelConsumption = Math.max(Double.parseDouble(matcher_2.group(1)), Double.parseDouble(matcher_2.group(2)));
+                    }
+                }
+
+
+                Car car = new Car(name, price, mileage, fuelConsumption);
                 cars.add(car);
             }
-        }catch (IOException | CsvException e) {
-            System.out.println("Error occurred while reading the files!");
+        }catch (NullPointerException e) {
+            System.out.println("Error! Null Pointer Exception occurred while reading the files!");
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Error! Number Format Exception occurred while reading the files!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error! IO Exception occurred while reading the files!");
+            e.printStackTrace();
+        } catch (CsvException e) {
+            System.out.println("Error! CSV Exception occurred while reading the files!");
             e.printStackTrace();
         }
 
@@ -148,6 +225,37 @@ public class BestCarDeal {
         for (Car car : cars) {
             System.out.println(car);
         }
+    }
+}
+
+class Car {
+    private String name;
+    private double price;
+    private String mileage;
+    private double fuelConsumption;
+
+    public Car(String name, double price, String mileage, double fuelConsumption) {
+        this.name = name;
+        this.price = price;
+        this.mileage = mileage;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public String getName() { return name; }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public double getFuelConsumption() { return fuelConsumption; }
+
+    @Override
+    public String toString() {
+        return name + " | $" + price + " | " + mileage + "km | " + fuelConsumption + " L/100km\n";
     }
 }
 
