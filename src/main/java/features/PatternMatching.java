2891c348a5724dd4260d148abe0d8aa58bdf8244
tasnim-farhin <40964564+tasnim-farhin@users.noreply.github.com>
2023-12-04 05:17:48 -0500

    Update PatternMatching.java

--- src/main/java/features/PatternMatching.java
+++ src/main/java/features/PatternMatching.java
@@ -1,34 +1,33 @@
 package features;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class PatternMatching {
     public static void main(String[] args) {
         // Directory containing files
-        String pathOfDir = "src/main/resources";
+        String pathToDir = "C:\\Users\\nikit\\OneDrive\\Desktop\\ACC Final project\\src";
 
-        // Define regex patterns for phone numbers and email addresses
-        String regexForPhone = "\\b\\d{3}[-.]?\\d{3}[-.]?\\d{4}\\b";
+        // Define regex patterns for phone numbers, emails and prices
+        String regexOfrPhone = "\\b\\d{3}[-.]?\\d{3}[-.]?\\d{4}\\b";
         String regexForEmail = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b";
-        String regexForPrice = "\\b\\$\\d+\\b";
-        // Creating Pattern objects
-        Pattern patternOfPhone = Pattern.compile(regexForPhone);
+        String regexForPrice = "\\$\\d+,\\d+";
+
+        // Create Pattern objects
+        Pattern patternOfPhone = Pattern.compile(regexOfrPhone);
         Pattern patternOfEmail = Pattern.compile(regexForEmail);
         Pattern patternOfPrice = Pattern.compile(regexForPrice);
 
         // Process each file in the directory
-        File directory = new File(pathOfDir);
+        File directory = new File(pathToDir);
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
-                if (file.isFile() && file.getName().endsWith(".csv")) {
+                if (file.isFile()) {
                     System.out.println("Searching in file: " + file.getName());
                     searchInFile(file, patternOfPhone, patternOfEmail, patternOfPrice);
                 }
@@ -39,40 +38,29 @@ public class PatternMatching {
     }
 
     public static void searchInFile(File file, Pattern patternOfPhone, Pattern patternOfEmail, Pattern patternOfPrice) {
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(file);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matchForPhone = patternOfPhone.matcher(line);
+                while (matchForPhone.find()) {
+                    System.out.println("Phone number found: " + matchForPhone.group());
+                }
 
-            // Traverse the document
-            traverseDoc(document, patternOfPhone, patternOfEmail);
-        } catch (Exception e) {
-            System.err.println("Error parsing the file: " + file.getName());
+                Matcher matchForEmail = patternOfEmail.matcher(line);
+                while (matchForEmail.find()) {
+                    System.out.println("Email address found: " + matchForEmail.group());
+                }
+                
+                Matcher matchForPrice = patternOfPrice.matcher(line);
+                while (matchForPrice.find()) {
+                    System.out.println("Prices found: " + matchForPrice.group());
+                }
+                
+                
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + file.getName());
             e.printStackTrace();
-        }
-    }
-
-    public static void traverseDoc(Node node, Pattern patternOfPhone, Pattern patternOfEmail, Pattern patternOfPrice) {
-        if (node.getNodeType() == Node.ELEMENT_NODE) {
-            NodeList childNodes = node.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                Node childNode = childNodes.item(i);
-                traverseDoc(childNode, patternOfPhone, patternOfEmail, patternOfPrice);
-            }
-        } else if (node.getNodeType() == Node.TEXT_NODE) {
-            String allText = node.getTextContent();
-            Matcher matchForPhone = patternOfPhone.matcher(allText);
-            while (matchForPhone.find()) {
-                System.out.println("Phone number found: " + matchForPhone.group());
-            }
-
-            Matcher matchForEmail = patternOfEmail.matcher(allText);
-            while (matchForEmail.find()) {
-                System.out.println("Email address found: " + matchForEmail.group());
-            }
-            Matcher matchForPrice = patternOfPrice.matcher(textContent);
-            while (emailMatcher.find()) {
-                System.out.println("Prices found: " + matchForPrice.group());}
         }
     }
 }
