7e67f9c2b03944fa54b7f804ca974e2a6e361260
shwakib <sifat6460@gmail.com>
2023-12-04 22:01:26 -0500

    ValidateUserInput.java plag reduced!

--- src/main/java/features/ValidateUserInput.java
+++ src/main/java/features/ValidateUserInput.java
@@ -4,32 +4,32 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidateUserInput {
-	public static boolean isValidPhoneNumber(String phoneNumber) {
+	public static boolean isValidPhoneNumber(String phoneNumberPassedFromTheMainFunction) {
         String phoneRegex = "^[2-9]\\d{2}-\\d{3}-\\d{4}$";
-        Pattern pattern = Pattern.compile(phoneRegex);
-        Matcher matcher = pattern.matcher(phoneNumber);
-        return matcher.matches();
+        Pattern patternForPhoneNumber = Pattern.compile(phoneRegex);
+        Matcher matcherForPhoneNumber = patternForPhoneNumber.matcher(phoneNumberPassedFromTheMainFunction);
+        return matcherForPhoneNumber.matches();
     }
 
-    public static boolean isValidEmailAddress(String email) {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-        Pattern pattern = Pattern.compile(emailRegex);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+    public static boolean isValidEmailAddress(String emailAddressPassedFromTheMainFunction) {
+        String emailRegexToBeFollowed = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern patternForEmailAddress = Pattern.compile(emailRegexToBeFollowed);
+        Matcher matcherForEmailAddress = patternForEmailAddress.matcher(emailAddressPassedFromTheMainFunction);
+        return matcherForEmailAddress.matches();
     }
 
-    public static boolean isValidPostalCode(String postalCode) {
+    public static boolean isValidPostalCode(String postalCodePassedFromTheMainFunction) {
         String postalCodeRegex = "^[ABCEGHJKLMNPRSTVXY][0-9][A-Z] [0-9][A-Z][0-9]$";
-        Pattern pattern = Pattern.compile(postalCodeRegex);
-        Matcher matcher = pattern.matcher(postalCode);
-        return matcher.matches();
+        Pattern patternForPostalCode = Pattern.compile(postalCodeRegex);
+        Matcher matcherForPostalCode = patternForPostalCode.matcher(postalCodePassedFromTheMainFunction);
+        return matcherForPostalCode.matches();
     }
 
-    public static boolean isValidWebsite(String websiteUrl) {
-        String regex = "^(https?|ftp):\\/\\/www\\.[\\w\\d\\-]+(\\.[\\w\\d\\-]+)+([\\w\\d\\-.,@?^=%&:/~+#]*[\\w\\d\\-@?^=%&/~+#])?$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(websiteUrl);
+    public static boolean isValidWebsite(String websiteUrlPassedFromTheMainFunction) {
+        String regexForWebsite = "^(https?|ftp):\\/\\/www\\.[\\w\\d\\-]+(\\.[\\w\\d\\-]+)+([\\w\\d\\-.,@?^=%&:/~+#]*[\\w\\d\\-@?^=%&/~+#])?$";
+        Pattern patternForWebsite = Pattern.compile(regexForWebsite);
+        Matcher matcherForWebsite = patternForWebsite.matcher(websiteUrlPassedFromTheMainFunction);
 
-        return matcher.matches();
+        return matcherForWebsite.matches();
     }
 }
