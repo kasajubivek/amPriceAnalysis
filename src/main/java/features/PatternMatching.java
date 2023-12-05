0f716f901e5ae10983ec68260d8599aefc680534
tasnim-farhin <40964564+tasnim-farhin@users.noreply.github.com>
2023-12-04 22:32:46 -0500

    Update PatternMatching.java

--- src/main/java/features/PatternMatching.java
+++ src/main/java/features/PatternMatching.java
@@ -1,67 +1,57 @@
 package features;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PatternMatching {
     public static void main(String[] args) {
-        // Directory containing files
-        String pathToDir = "src/main/resources";
-
-        // Define regex patterns for phone numbers, emails and prices
-        String regexOfrPhone = "\\b\\d{3}[-.]?\\d{3}[-.]?\\d{4}\\b";
-        String regexForEmail = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b";
+        // Showing path of directory
+        String pathToDir = "C:\\Users\\nikit\\OneDrive\\Desktop\\Advanced Computing Concepts\\Documents\\GitHub\\acc_project\\src\\main\\resources";
+        // Define regex patterns for phone numbers and prices
+        String regexOfrPhone = "\\b[0-9]{3}[-.]?[0-9]{3}[-.]?[0-9]{4}\\b";
+      
         String regexForPrice = "\\$\\d+,\\d+";
 
-        // Create Pattern objects
+        // Creating objects for Pattern
         Pattern patternOfPhone = Pattern.compile(regexOfrPhone);
-        Pattern patternOfEmail = Pattern.compile(regexForEmail);
         Pattern patternOfPrice = Pattern.compile(regexForPrice);
 
-        // Process each file in the directory
+        // Processing all the files present in the  directory
         File dir = new File(pathToDir);
-        File[] files = dir.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isFile()) {
-                    System.out.println("Searching in file: " + file.getName());
-                    searchInFile(file, patternOfPhone, patternOfEmail, patternOfPrice);
+        File[] filesFound = dir.listFiles();
+        if (filesFound != null) {
+            for (File filesHere : filesFound) {
+                if (filesHere.isFile()) {
+                    System.out.println("Reading in file: " + filesHere.getName());
+                    searchFile(filesHere, patternOfPhone, patternOfPrice);
                 }
             }
         } else {
-            System.out.println("Invalid directory path.");
+            System.out.println("Invalid directory path. Check or try a different path");
         }
     }
 
-    public static void searchInFile(File file, Pattern patternOfPhone, Pattern patternOfEmail, Pattern patternOfPrice) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                Matcher matchForPhone = patternOfPhone.matcher(line);
+    public static void searchFile(File filesFound, Pattern patternOfPhone, Pattern patternOfPrice) {
+        try (BufferedReader bufReaddd = new BufferedReader(new FileReader(filesFound))) {
+            String data;
+            while ((data = bufReaddd.readLine()) != null) {
+                Matcher matchForPhone = patternOfPhone.matcher(data);
                 while (matchForPhone.find()) {
-                    System.out.println("Phone number found: " + matchForPhone.group());
+                    System.out.println("Phone numbers in the file found: " + matchForPhone.group());
                 }
 
-                Matcher matchForEmail = patternOfEmail.matcher(line);
-                while (matchForEmail.find()) {
-                    System.out.println("Email address found: " + matchForEmail.group());
-                }
                 
-                Matcher matchForPrice = patternOfPrice.matcher(line);
+                Matcher matchForPrice = patternOfPrice.matcher(data);
                 while (matchForPrice.find()) {
-                    System.out.println("Prices found: " + matchForPrice.group());
+                    System.out.println("Prices of car found: " + matchForPrice.group());
                 }
                 
                 
             }
-        } catch (IOException e) {
-            System.err.println("Error reading the file: " + file.getName());
-            e.printStackTrace();
-        }
+        } catch (IOException e) {System.out.println("Problem with readind files");}
+          
     }
 }
 
